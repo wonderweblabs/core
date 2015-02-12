29186a5554015796bb52b23e6320181eb04d8838
Sascha Hillig <sascha@wonderweblabs.com>
2015-02-12 11:15:06 +0100

    Fix current_user issue
    - if using devise in parallel - current_user resolves the devise user sometimes

--- app/controllers/shoppe/application_controller.rb
+++ app/controllers/shoppe/application_controller.rb
@@ -1,12 +1,12 @@
 module Shoppe
   class ApplicationController < ActionController::Base
-    
+
     before_filter :login_required
-    
+
     rescue_from ActiveRecord::DeleteRestrictionError do |e|
       redirect_to request.referer || root_path, :alert => e.message
     end
-    
+
     rescue_from Shoppe::Error do |e|
       @exception = e
       render :layout => 'shoppe/sub', :template => 'shoppe/shared/error'
@@ -21,26 +21,26 @@ module Shoppe
     end
 
     def logged_in?
-      current_user.is_a?(User)
+      current_user.is_a?(Shoppe::User)
     end
-    
+
     def current_user
       @current_user ||= login_from_session || login_with_demo_mdoe || :false
     end
 
     def login_from_session
       if session[:shoppe_user_id]
-        @user = User.find_by_id(session[:shoppe_user_id])
+        @user = Shoppe::User.find_by_id(session[:shoppe_user_id])
       end
     end
-    
+
     def login_with_demo_mdoe
       if Shoppe.settings.demo_mode?
-        @user = User.first
+        @user = Shoppe::User.first
       end
     end
-    
+
     helper_method :current_user, :logged_in?
-    
+
   end
 end
